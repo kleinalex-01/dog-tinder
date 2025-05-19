e9e13aa7df3ba44407c17fcd621e472d6b65f94b
kleinalex-01 <kleinalex215@gmail.com>
2025-05-19 16:54:53 +0200

    App.js swipe func
    Messing around with framer motion, trying out different "swipe" logics.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,89 @@
-import React, {useState, useEffect} from "react"
+import React, {useState, useEffect, useRef} from "react"
 import axios from "axios";
 import './App.css';
 import Header from './Components/Header';
+import { motion, useAnimation, useMotionValue, useScroll, useTransform } from "motion/react";
 function App() {
-  const [data, setData] = useState([])
-  const [dataIndex, setDataIndex] = useState(0)
-
-  function swipe() {
-    setDataIndex(prev => prev + 1)
-  }
+  const [dogPics, setDogPics] = useState([])
+  const [nextPic, setNextPic] = useState(false)
+  const x = useMotionValue(0);
+  const controls = useAnimation();
+  const rotate = useTransform(x, [-200, 200], [-15, 15]);
 
   useEffect(() => {
-    axios.get("/data.json")
+    axios.get("https://dog.ceo/api/breeds/image/random")
       .then(res => {
-        setData(res.data);
+        setDogPics(res.data);
       })
       .catch(err => {
         console.error(`An error occured getting the JSON: ${err}`)
       })
   }, [])
 
-  const dog = data[dataIndex];
+  function swipePic() {
+    axios.get("https://dog.ceo/api/breeds/image/random")
+      .then(res => {
+        setDogPics(res.data);
+      })
+      .catch(err => {
+        console.error(`An error occured getting the JSON: ${err}`)
+      })
+  }
 
-  if (!dog) return <p>Loading...</p>
+  function handleDragEnd(event, info) {
+    if (info.offset.x > 100) {
+      swipePic()
+      setNextPic(true)
+      controls.start({
+        x: 1000,
+        opacity: 0,
+        transition: { duration: 0.5 }
+      });
+    }
+    if (info.offset.x < -100) {
+      swipePic()
+    }
+  }
+
+
+  if (!dogPics) return <p>Loading...</p>
 
   return (
     <>
       <Header />
       <div className='content-container'>
-          <div className='card-container' style={{ backgroundImage: `url(${dog.picture})`,
-                                                  backgroundSize: "cover",
-                                                  backgroundPosition: "center",
-                                                  backgroundRepeat: "no-repeat"
-                                                    }}>
-              <h1 className="username">{dog.name}, {dog.age}</h1>
-              <p className="userBio">{dog.bio}</p>
-          </div>
+        <motion.div
+            className="card-container"
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            drag="x"
+            style={{
+              x,
+              rotate,
+              backgroundImage: `url(${dogPics.message})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat"
+            }}
+            dragConstraints={{ left: 0, right: 0 }}
+            whileTap={{ scale: 0.9 }}
+            onDragEnd={(event, info) => {
+              if (info.offset.x > 100) {
+                swipePic()
+              }
+              if (info.offset.x < -100) {
+                swipePic()
+              }
+            }}
+          >
+            <div className="card-overlay">
+            <h1 className="username">chad, 30</h1>
+            <p className="userBio">hot dog</p>
+            </div>
+          </motion.div>
         <div className='hit-or-miss-container'>
-          <button className='hit-or-miss-btn' onClick={swipe}><img src='../Images/miss.png' alt='miss button' className='hit-or-miss-icon'/></button>
-          <button className='hit-or-miss-btn' onClick={swipe}><img src='../Images/hit.png' alt='hit button' className='hit-or-miss-icon'/></button>
+          <button onClick={swipePic} className='hit-or-miss-btn'><img src='../Images/miss.png' alt='miss button' className='hit-or-miss-icon'/></button>
+          <button onClick={swipePic} className='hit-or-miss-btn'><img src='../Images/hit.png' alt='hit button' className='hit-or-miss-icon'/></button>
         </div>
       </div>
     </>
